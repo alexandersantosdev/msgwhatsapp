0bb70d7c41cc5defced51e08cd88b005af563ad2
unknown <lekxandy@hotmail.com>
2021-07-29 13:01:54 -0300

    Sistema terminado

---
+++ README.md
@@ -0,0 +1,5 @@
+# Envie mensagens de whatsapp sem a necessidade de salvar o contato
+
+### Utilizando simplemente html5, CSS e JavaScript
+
+![Tela principal](https://github.com/alexandersantosdev/msgwhatsapp/blob/main.png)
---
+++ js/script.js
@@ -0,0 +1,29 @@
+var dados = {
+    pais: '',
+    ddd: '',
+    numero: ''
+}
+
+var contato = ''
+
+const setDados = (valor, name) => {
+    if (valor == null || isNaN(valor)) {
+        alert("Digite um numero válido")
+        document.querySelector(`#${name}`).value = ''
+        valor = ''
+        return
+    }
+
+    dados[name] = valor
+    const { pais, ddd, numero } = dados
+    contato = `${pais}${ddd}${numero}`
+}
+
+const navegar = () => {
+    if (contato == null || contato == "") {
+        alert("Preencha os campos corretamente")
+        return
+    }
+    document.location.href = `https://api.whatsapp.com/send?phone=${contato}`
+
+}
